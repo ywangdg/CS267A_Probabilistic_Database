a5b57066fc5a7c151d11c429e4412e9c57ba126f
ywangdg <ywangdg@connect.ust.hk>
2018-11-14 14:39:41 -0800

    command line reader for qeval.py

--- qeval.py
+++ qeval.py
@@ -1,6 +1,18 @@
 import sys, getopt
 
 
+database = dict()
+#
+# def generate_table(tables):
+#     for table in tables:
+#
+
+def read_table(table):
+    fd = open(table, "r")
+    table_name = fd[0]
+    print table_name
+
+
 def main(argv):
     queries = []
     tables = []
@@ -20,8 +32,7 @@ def main(argv):
     print 'Input query file is', queries
     print 'Input table file is', tables
 
-
-
+    read_table(tables[0])
 
 if __name__ == "__main__":
    main(sys.argv[1:])
