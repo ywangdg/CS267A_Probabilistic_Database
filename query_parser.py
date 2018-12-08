42e1449449d15c3ddf9fe73b4c9e257d99481937
Michael Xiong <mmxiong@ucla.edu>
2018-12-08 15:01:21 -0800

    add variable name to column name mapping in query

--- query_parser.py
+++ query_parser.py
@@ -5,6 +5,27 @@ class Query:
     def __init__(self,tables, variables):
         self.tables = tables
         self.variables = variables
+        # Create a list of mappings for each clause.
+        # Each mapping is of the form:
+        # {
+        #     table_name : {
+        #         var_name : column_name
+        #     }
+        # }
+        # table_name is the name of the table in our database.
+        # var_name is the name of the variable in our query (self.variables)
+        # column_name is the column name that the variable refers to.
+        self.variable_column_mapping_list = []
+        for clause_num in range(len(self.tables)):
+            table_to_mapping = {}
+            for table_num in range(len(self.tables[clause_num])):
+                table_name = self.tables[clause_num][table_num]
+                variable_to_column_mapping = {}
+                for variable_num in range(len(self.variables[clause_num][table_num])):
+                    variable_name = self.variables[clause_num][table_num][variable_num]
+                    variable_to_column_mapping[variable_name] = "var{}".format(variable_num + 1)
+                table_to_mapping[table_name] = variable_to_column_mapping
+            self.variable_column_mapping_list.append(table_to_mapping)
 
 def parse_query(query):
     file = open(query, "r")
